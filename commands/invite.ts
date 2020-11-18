138b06c4696d846863bff29240b5b33461575c5e
Chr1sDev <chris@chr1s.dev>
2020-11-18 12:45:03 -0600

    add //invite

---
+++ commands/invite.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    name:"invite",
+    execute(msg) {
+
+        msg.channel.send(`Bot Invite Link:\n\<https://discord.com/oauth2/authorize?client_id=763464598959292458&scope=bot&permissions=515136\>`);
+
+    },
+};
