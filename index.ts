5fc5ff1f0e69312abe3cc99e0e1d22fcc714f5ba
Chr1s <32487758+Chr1sDev@users.noreply.github.com>
2020-11-18 13:12:12 -0600

    hehe

--- index.ts
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.ts', { token: 'redacted' });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
