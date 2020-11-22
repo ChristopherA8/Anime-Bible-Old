4a04ccfa28f97bf54711f58f6e53aa76b9a14575
Chr1sDev <chris@chr1s.dev>
2020-11-21 21:02:35 -0600

    update status every couple minutes

--- bot.ts
+++ bot.ts
@@ -30,7 +30,10 @@ client.on('ready', () => {
   // Sets Bot Status
   console.log("Connected as " + client.user.tag + ", yo yo yo")
   //client.user.setActivity("with JavaScript", {type: "PLAYING"})
-  client.user.setActivity(`//help in ${client.guilds.cache.size} servers`, {type: "PLAYING"})
+  setInterval(() => {
+    client.user.setActivity(`//help in ${client.guilds.cache.size} servers`, {type: "PLAYING"})
+  }, 1800000);
+
 
   //top.gg
   setInterval(() => {
