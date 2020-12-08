af6bfb52a3224d52fda715bfbb5564503e3983c5
Chr1sDev <chris@chr1s.dev>
2020-12-07 23:07:29 -0600

    add stats database

---
+++ commands/create.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    name:"create",
+    execute(msg) {
+
+        const SQLite = require('better-sqlite3');
+
+    // Create SQLite database
+    const sql = new SQLite('./databases/stats.sqlite');
+
+        // If the table isn't there, create it and setup the database correctly.
+        sql.prepare("CREATE TABLE stats (total INTEGER, anime INTEGER, manga INTEGER, character INTEGER, help INTEGER, about INTEGER, invite INTEGER, quote INTEGER);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
+
+    },
+};
---
+++ commands/names.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    name:"names",
+    execute(msg) {
+
+        //msg.client.guilds.cache.forEach(server => console.log(server.name))
+        // var name = msg.client.guilds.cache.filter(server => server.partnered == true);
+        // console.log(name.name);
+
+    },
+};
